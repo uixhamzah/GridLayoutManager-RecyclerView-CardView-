e3ff7e62b36259ecc4a02a1d613a17169b30935c
uixhamzah <uixhamzah@gmail.com>
2021-12-03 20:47:25 +0700

    Task1

---
+++ README.md
@@ -0,0 +1,1 @@
+# GridLayoutManager-RecyclerView-CardView-
---
+++ app/src/main/java/com/basic/mygridlayoutapps/model/AlphaChar.kt
@@ -0,0 +1,12 @@
+package com.basic.mygridlayoutapps.model
+
+class AlphaChar {
+
+    var iconsChar: Int ? = 0
+    var alphaChar: String ? = null
+
+    constructor(iconsChar: Int?, alphaChar: String?) {
+        this.iconsChar = iconsChar
+        this.alphaChar = alphaChar
+    }
+}
